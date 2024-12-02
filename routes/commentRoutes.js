84cbb9c0dba948fa9f02ece8eb47bac04d6ba692
eladrab1106 <eladrabinovitch1106@gmail.com>
2024-12-02 18:31:11 +0200

    createComment

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -3,3 +3,6 @@ const router= express.Router();
 const commentController = require('../controllers/comment');
 // Get all comments
 router.get('/', commentController.getAllComments);
+
+// Create a new comment
+router.post('/', commentController.createComment);
