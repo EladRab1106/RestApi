3b731f558dc50a9009010112bb17b6323b05bd0d
roiyraz88 <roiyraz88@gmail.com>
2025-01-01 22:06:26 +0200

    Fixed the tests.

--- tests/comments.test.js
+++ tests/comments.test.js
@@ -1,76 +1,97 @@
-const request = require('supertest');
-const app = require('../server');
-const mongoose = require('mongoose');
-const commentModel = require('../models/commentModel');
-const postsModel = require('../models/postModel');
-comments=[{
-    title: 'first post',
-    content: 'this is the first post',
-    postId: '1',
-    owner: 'elad'
-},{
-    title: 'second post',
-    content: 'this is the second post',
-    postId: '2',
-    owner: 'roie'
-}]
+const request = require("supertest");
+const app = require("../server");
+const mongoose = require("mongoose");
+const commentModel = require("../models/commentModel");
+const postsModel = require("../models/postModel");
+
+const comments = [
+  {
+    title: "first comment",
+    content: "this is the first comment",
+    postId: new mongoose.Types.ObjectId(),
+    owner: "elad",
+  },
+  {
+    title: "second comment",
+    content: "this is the second comment",
+    postId: new mongoose.Types.ObjectId(),
+    owner: "roie",
+  },
+  {
+    title: "third comment",
+    content: "this is the third comment",
+    postId: new mongoose.Types.ObjectId(),
+    owner: "eliav",
+  },
+];
 
 beforeAll(async () => {
-    console.log('beforeAll')
-    await commentModel.deleteMany();
-})
+  console.log("beforeAll");
+  await commentModel.deleteMany();
+  await commentModel.insertMany(comments); // Seed the comments
+});
 
-afterAll(() => {
-    console.log('afterAll')
-    mongoose.connection.close();
-})
+afterAll(async () => {
+  console.log("afterAll");
+  await mongoose.connection.close();
+});
 
-describe('comments', () => {
-    test('should get all comments',async () => {
-        const response = await request(app).get('/comment');
-        expect(response.statusCode).toBe(200);
-        expect(response.body.length).toBe(0);
+describe("comments", () => {
+  test("should get all comments", async () => {
+    const response = await request(app).get("/comment");
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBe(comments.length); // Ensure the seeded data is returned
+  });
+
+  test("should create new comments", async () => {
+    const newComment = {
+      title: "fourth comment",
+      content: "this is the fourth comment",
+      postId: new mongoose.Types.ObjectId().toString(),
+      owner: "dan",
+    };
+    const response = await request(app).post("/comment").send(newComment);
+    expect(response.statusCode).toBe(201);
+    expect(response.body.title).toBe(newComment.title);
+    expect(response.body.content).toBe(newComment.content);
+    expect(response.body.owner).toBe(newComment.owner);
+    expect(response.body.postId).toBe(newComment.postId);
+  });
+
+  test("should update a comment by id", async () => {
+    const comment = await commentModel.findOne();
+    const response = await request(app)
+      .put(`/comment/${comment._id}`)
+      .send({ title: "updated title" });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.title).toBe("updated title");
+  });
+
+  test("should find a comment by post id", async () => {
+    const post = await postsModel.create({
+      title: "first post",
+      content: "this is the first post",
+      owner: "elad",
     });
-    
-    test('should create a new post', async () => {
-        const post = await postsModel.create({title: 'first post', content: 'this is the first post', owner: 'elad'});
-        const response = await request(app).post('/comment').send({title: 'first comment', content: 'this is the first comment', postId: post._id, owner: 'elad'});
-        expect(response.statusCode).toBe(201);
-        expect(response.body.title).toBe('first comment');
-        expect(response.body.content).toBe('this is the first comment');
-        expect(response.body.owner).toBe('elad');
-        expect(response.body.postId).toBe(post._id.toString());
-
-    
-    })
-
-    test('should update a comment by id', async () => {
-
-        const post = await postsModel.create({title: 'first post', content: 'this is the first post', owner: 'elad'});
-        const comment = await commentModel.create({title: 'first comment', content: 'this is the first comment', postId: post._id, owner: 'elad'});
-        const response = await request(app).put(`/comment/${comment._id}`).send({title: 'updated title'});
-        expect(response.statusCode).toBe(200);
-        expect(response.body.title).toBe('updated title');
+    const comment = await commentModel.create({
+      title: "first comment",
+      content: "this is the first comment",
+      postId: post._id,
+      owner: "roie",
     });
+    const response = await request(app).get(`/comment/${post._id}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body[0].title).toBe(comment.title);
+    expect(response.body[0].content).toBe(comment.content);
+    expect(response.body[0].owner).toBe(comment.owner);
+    expect(response.body[0].postId).toBe(post._id.toString());
+  });
 
-    test('should find a comment by post id', async () => {
-        const post = await postsModel.create({title: 'first post', content: 'this is the first post', owner: 'elad'});
-        const comment = await commentModel.create({title: 'first comment', content: 'this is the first comment', postId: post._id, owner: 'elad'});
-        const response = await request(app).get(`/comment/${post._id}`);
-        expect(response.statusCode).toBe(200);
-        expect(response.body[0].title).toBe('first comment');
-        expect(response.body[0].content).toBe('this is the first comment');
-        expect(response.body[0].owner).toBe('elad');
-        expect(response.body[0].postId).toBe(post._id.toString());
-    });
-
-    test('should delete a comment by id', async () => {
-        const post = await postsModel.create({title: 'first post', content: 'this is the first post', owner: 'elad'});
-        const comment = await commentModel.create({title: 'first comment', content: 'this is the first comment', postId: post._id, owner: 'elad'});
-        const response = await request(app).delete(`/comment/${comment._id}`);
-        expect(response.statusCode).toBe(200);
-    });
-
-})
-
-    
+  test("should delete a comment by id", async () => {
+    const comment = await commentModel.findOne();
+    const response = await request(app).delete(`/comment/${comment._id}`);
+    expect(response.statusCode).toBe(200);
+    const deletedComment = await commentModel.findById(comment._id);
+    expect(deletedComment).toBeNull();
+  });
+});
--- tests/posts.test.js
+++ tests/posts.test.js
@@ -1,74 +1,75 @@
-const request = require('supertest');
-const app = require('../server');
-const mongoose = require('mongoose');
-const postModel = require('../models/postModel');
+const request = require("supertest");
+const app = require("../server");
+const mongoose = require("mongoose");
+const postModel = require("../models/postModel");
 
-posts=[{
-    title: 'first post',
-    content: 'this is the first post',
-    owner: 'elad'
-},{
-    title: 'second post',
-    content: 'this is the second post',
-    owner: 'roie'
-}]
+const posts = [
+  { title: "first post", content: "this is the first post", owner: "elad" },
+  { title: "second post", content: "this is the second post", owner: "roie" },
+  { title: "third post", content: "this is the third post", owner: "eliav" },
+];
 
 beforeAll(async () => {
-    console.log('beforeAll')
-    await postModel.deleteMany();
-})
+  console.log("beforeAll");
+  await postModel.deleteMany(); // Clear the collection
+  await postModel.insertMany(posts); // Seed the database with test data
+});
 
-afterAll(() => {
-    console.log('afterAll')
-    mongoose.connection.close();
-})
+afterAll(async () => {
+  console.log("afterAll");
+  await mongoose.connection.close(); // Close the connection
+});
 
-describe('posts', () => {
-    test('should get all posts',async () => {
-        const response = await request(app).get('/post');
-        expect(response.statusCode).toBe(200);
-        expect(response.body.length).toBe(0);
-    });
-    
-    test('should create a new post', async () => {
-        const response = await request(app).post('/post').send(posts[0]);
-        expect(response.statusCode).toBe(201);
-        expect(response.body.title).toBe(posts[0].title);
-        expect(response.body.content).toBe(posts[0].content);
-        expect(response.body.owner).toBe(posts[0].owner);
+describe("posts", () => {
+  test("should get all posts", async () => {
+    const response = await request(app).get("/post");
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBe(posts.length); // Should match the number of seeded posts
+  });
 
-    
-    })
+  test("should create a new post", async () => {
+    const newPost = {
+      title: "fourth post",
+      content: "this is the fourth post",
+      owner: "dan",
+    };
+    const response = await request(app).post("/post").send(newPost);
+    expect(response.statusCode).toBe(201);
+    expect(response.body.title).toBe(newPost.title);
+    expect(response.body.content).toBe(newPost.content);
+    expect(response.body.owner).toBe(newPost.owner);
+  });
 
-    test('should get a post by id', async () => {
-        const post = await postModel.create(posts[1]);
-        const response = await request(app).get(`/post/${post._id}`);
-        expect(response.statusCode).toBe(200);
-        expect(response.body.title).toBe(posts[1].title);
-        expect(response.body.content).toBe(posts[1].content);
-        expect(response.body.owner).toBe(posts[1].owner);
-    })
+  test("should get a post by id", async () => {
+    const post = await postModel.findOne({ title: "first post" }); // Find one post from the database
+    const response = await request(app).get(`/post/${post._id}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.title).toBe(post.title);
+    expect(response.body.content).toBe(post.content);
+    expect(response.body.owner).toBe(post.owner);
+  });
 
-    test('should update a post by id', async () => {
-        const post = await postModel.create(posts[1]);
-        const response = await request(app).put(`/post/${post._id}`).send({title: 'updated title'});
-        expect(response.statusCode).toBe(200);
-        expect(response.body.title).toBe('updated title');
-    })
+  test("should update a post by id", async () => {
+    const post = await postModel.findOne({ title: "second post" });
+    const response = await request(app)
+      .put(`/post/${post._id}`)
+      .send({ title: "updated title" });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.title).toBe("updated title");
+  });
 
-    test('should delete a post by id', async () => {
-        const post = await postModel.create(posts[1]);
-        const response = await request(app).delete(`/post/${post._id}`);
-        expect(response.statusCode).toBe(200);
-    })
+  test("should delete a post by id", async () => {
+    const post = await postModel.findOne({ title: "third post" });
+    const response = await request(app).delete(`/post/${post._id}`);
+    expect(response.statusCode).toBe(200);
+    const deletedPost = await postModel.findById(post._id); // Verify it's deleted
+    expect(deletedPost).toBeNull();
+  });
 
-    test('should get all posts by owner', async () => {
-        const response = await request(app).get('/post?owner=elad');
-        expect(response.statusCode).toBe(200);
-        expect(response.body.length).toBe(1);
-    })
-
-
-})
-
-    
+  test("should get all posts by owner", async () => {
+    const response = await request(app).get("/post?owner=elad");
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBe(1); // Only one post with owner "elad"
+    expect(response.body[0].owner).toBe("elad");
+  });
+});
